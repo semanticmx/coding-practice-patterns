be144c30259d1ce6ddbcc62b16f676dad9f7d88f
Carlos Vences <carlos@semantic.mx>
2025-06-03 17:34:30 -0600

    feat: fixed test_clean_word

--- string-manipulation/remove-special-chars/main.py
+++ string-manipulation/remove-special-chars/main.py
@@ -5,25 +5,34 @@ class Solution:
     def strip_nonalnum(self, word):
         """
         $green => green
+        !!baby! => baby
         """
         # .isalnum(): bool
         # "asd".isalnum() => True
         # "#asd".isalnum() => False
         # short-circuit function
+        # O(n^2)
         if word.isalnum():
             return word
 
-        cleaned = word
-        if not word[0].isalnum():
-            cleaned = word[1:]
+        # O(2n) - storage complexity
+        chars = list(word)
+        # !!baby!
+        # !baby!
+        # baby!
+        # O(n)
+        while not chars[0].isalnum():
+            del chars[0]
 
-        if not cleaned[-1].isalnum():
-            # python ranges are [0:6)
-            cleaned = cleaned[:-1]
+        # O(n)
+        while not chars[-1].isalnum():
+            del chars[-1]
 
-        return cleaned
+        # O(n)
+        return "".join(chars)
 
     def convert_to_words(self, text:str)->list:
+        # O(n)
         # 2.
         # words = text.split()
         # for word in words:
@@ -33,11 +42,18 @@ class Solution:
         return text.split()
 
     def execute(self, text:str)->dict:
+        # O(n)
         words = self.convert_to_words(text=text)
+        # O(c)
         stats = defaultdict(int)
+        # O(n)
         for word in words:
             # 1. word = word.lower() [x]
+            # O(n)
             word = word.lower()
+            # O(2n + n^2)
             word = self.strip_nonalnum(word)
+            # O(c)
             stats[word] += 1
+        # O(n^3)
         return stats
--- string-manipulation/remove-special-chars/test_excercise.py
+++ string-manipulation/remove-special-chars/test_excercise.py
@@ -78,3 +78,43 @@ def test_special_chars_on_bounds():
     }
 
     assert result == expected
+
+
+def test_inner_special_chars():
+    text = "l3!!4d Cow baby! success cow b1,gg3r $gr33.n cake"
+    result = get_result(text=text)
+    expected = {
+        "l3!!4d": 1,
+        "cow": 2,
+        "baby": 1,
+        "success": 1,
+        "b1,gg3r": 1,
+        "gr33.n": 1,
+        "cake": 1,
+    }
+
+    assert result == expected
+
+
+def test_clean_word():
+    word = "!!baby!"
+    instance = Solution()
+    cleaned = instance.strip_nonalnum(word=word)
+
+    assert cleaned == cleaned
+
+
+def test_final_boss():
+    text = "\"l3\"4d\"\"\" Cow !!baby! succ..ess cow #b1,gg3,r $gr33.n# cake"
+    result = get_result(text=text)
+    expected = {
+        "l3\"4d": 1,
+        "cow": 2,
+        "baby": 1,
+        "succ..ess": 1,
+        "b1,gg3,r": 1,
+        "gr33.n": 1,
+        "cake": 1,
+    }
+
+    assert result == expected
