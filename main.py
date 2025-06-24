47ad4801e32adeda0d9f0111f8c7da7f76876439
Carlos Vences <carlos@semantic.mx>
2025-06-24 12:00:21 -0600

    feat: implemented gcd

---
+++ main.py
@@ -0,0 +1,11 @@
+from string_manipulation.gcd.main import gcd_from_strings
+
+def execute():
+    print("シ!")
+    gcd_from_strings("ABC", "ABCABCABC")
+    from cpp import strings
+    print(strings.utils.is_integer(3))
+
+
+if __name__ == "__main__":
+    execute()
--- string_manipulation/gcd/main.py
+++ string_manipulation/gcd/main.py
@@ -1,4 +1,5 @@
 import unittest
+import math
 
 
 def _cleaned_data(*args)->list:
@@ -36,12 +37,39 @@ def gcd_from_strings(str1: str, str2: str)->bool:
     8. Work after the work: support
 
     if str1 divides by GCD str2 then return True
+    str1 = "ABAB"
+    str2 = "AABBAABBAABBAABB"
+    Propuesta 1
+    1. Contar cada letra única de str1 & str2
+    2. Que sean iguales
+    3. Ver si la frecuencia de str2 es igual en cada letra
+
+    str3 = "AABB"
+    str4 = "AABBAABBAABBAABB"
+
+    "AABB" "AABBAABBAABBAABB"
+    "AABBAABBAABBAABB" "AABB"
+
+    Propuesta 2
+    1. Si str1 se repite en str2
+    2. Seccionar por bloques de str1
+
+    Propuesta 3
+    1. Sumar str1 + str2 y str2 + str1
+    2. Verificar que son iguales
 
     str1: divisor string
     str2: string to be divided
     :returns bool
     """
-    pass
+    if not all((str1, str2)):
+        return False
+
+    if str1 + str2 != str2 + str1:
+        return False
+
+    gcd = math.gcd(len(str1), len(str2))
+    return gcd == len(str1)
 
 
 class GcdTestCase(unittest.TestCase):
@@ -73,7 +101,7 @@ class GcdTestCase(unittest.TestCase):
         str1 = "A"
         str2 = "ABC"
         result = gcd_from_strings(str1, str2)
-        self.assertTrue(result)
+        self.assertFalse(result)
 
     def test_greater_divider(self):
         str1 = "ABCD"
@@ -128,6 +156,16 @@ class GcdTestCase(unittest.TestCase):
         self.assertEqual("ABCD", c1)
         self.assertEqual("ABCDABCDABCD", c2)
 
+    def test_is_gcd(self):
+        """
+        str1 = "ABCDEABCDEABCDE"
+        str2 = "ABCDEABCDEABCDEABCDE"
+
+        """
+        str1 = "ABCDEABCDEABCDE"
+        str2 = "ABCDEABCDEABCDEABCDE"
+        result = gcd_from_strings(str1, str2)
+        self.assertFalse(result)
 
 if __name__ == "__main__":
     unittest.main()
